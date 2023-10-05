e3726505188252159151afe8d8605be85cb39ffc
sashmins21 <sasha.minsky@gmail.com>
2023-10-05 10:46:44 -0400

    testing

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import tkinter as tk
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Create the main tkinter window
+root = tk.Tk()
+root.title("Pallanguzi")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hello")
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Start the tkinter main loop
+root.mainloop()
