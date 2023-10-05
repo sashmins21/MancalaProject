a6b723dc235fddbd06933b1cb7a449f38678b5dc
sashmins21 <sasha.minsky@gmail.com>
2023-10-05 11:16:07 -0400

    testing

--- main.py
+++ main.py
@@ -16,6 +16,20 @@ for i in range(rows):
         row_buttons.append(button)
     button_grid.append(row_buttons)
 
+for i in range(14):
+    btn = button_grid[i]
+    btn.place(x=210, y=210)
+
+
+# Initialize tkinter window with dimensions 300 x 250
+root.geometry('500x500')
+
+# Creating a Button
+btn = tk.Button(root, text="Store", height = 4, width=20)
+
+# Set the position of button to coordinate (100, 20)
+btn.place(x=210, y=210)
+
 
 
 # Start the tkinter main loop
