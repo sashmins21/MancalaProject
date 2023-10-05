10712499e69b490e7ac13acd1f48283ab54f117b
sashmins21 <sasha.minsky@gmail.com>
2023-10-05 10:30:11 -0400

    testing git

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print("Hello")
 
 
 # Press the green button in the gutter to run the script.
