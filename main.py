bf9625d224aaf0184500d2c6ce308052276acffe
sashmins21 <sasha.minsky@gmail.com>
2023-10-10 08:58:57 -0400

    testing

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Pallanguzhi")
 
+# Create a canvas widget
+canvas = tk.Canvas(root, width=400, height=300)
+canvas.pack()
+
 rows = 2
 cols = 7
 button_grid = []
@@ -11,17 +15,19 @@ button_grid = []
 for i in range(rows):
     row_buttons = []
     for j in range(cols):
-        button = tk.Button(root, text="", height=5, width=5)
+        button = tk.Button(root, text="B B B B", height=5, width=5)
         button.grid(row=i, column=j, padx=5, pady=5)
         row_buttons.append(button)
     button_grid.append(row_buttons)
+
+
 
 # Initialize tkinter window with dimensions 650 x 500
 root.geometry('650x500')
 
 # Creating a Button
-btn = tk.Button(root, text="Store", height=4, width=20)
-btn2 = tk.Button(root, text="Store", height=4, width=20)
+btn = tk.Button(root, text="", height=4, width=20)
+btn2 = tk.Button(root, text="", height=4, width=20)
 
 btn.place(x=100, y=210)
 btn2.place(x=325, y=210)
