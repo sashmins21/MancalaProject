ee2f2033a56e2214de750e2da852319ac0bdf11b
foxchristiansen <foxchristiansen@gmail.com>
2023-10-16 11:16:39 -0400

    rectangles

--- main.py
+++ main.py
@@ -34,9 +34,16 @@ btn2.place(x=325, y=200)
 
 def move(row, col):
     for i in range(len(button_grid[row * 7 + col].cget("text"))):
-        current_text = str(button_grid[row*7 +col-i-1].cget("text")) # FIX THIS
-        button_grid[row*7 + col-i-1].config(text =current_text + "O")
+        current_text = str(button_grid[row * 7 + col - i - 1].cget("text"))
+        button_grid[row * 7 + col - i - 1].config(text=current_text + "O")
+        if i == len(button_grid[row * 7 + col].cget("text")) - 1:
+            next_index = row * 7 + col - i
+            if next_index % 7 != 6 and len(button_grid[next_index + 1].cget("text")) == 0:
+                btn.config(text = "HERRO")
     button_grid[row * 7 + col].config(text="")
+
+
+
 
 
 
