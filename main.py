7a3de5aa021e39be23eb61b3bf6f9d29a13add48
foxchristiansen <foxchristiansen@gmail.com>
2023-10-18 09:13:16 -0400

    rectangles

--- main.py
+++ main.py
@@ -40,8 +40,17 @@ def move(row,col):
     for i in range(num_Os):
         current_text = str(button_grid[(current_index + i + 1) % num_buttons].cget("text"))
         button_grid[(current_index + i + 1) % num_buttons].config(text=current_text + "O")
-
-    button_grid[current_index].config(text="")
+        if i == len(button_grid[row * 7 + col].cget("text")) - 1:
+            next_index = row * 7 + col - i
+            if next_index % 7 != 6 and len(button_grid[next_index + 1].cget("text")) == 0:
+                prev_index = row * 7 + col - i - 1
+                while prev_index != next_index:
+                    current_text = str(button_grid[prev_index].cget("text"))
+                    btn_text = str(btn.cget("text"))
+                    btn.config(text=btn_text + current_text)
+                    button_grid[prev_index].config(text="")
+                    prev_index = (prev_index - 1) % 14  # Wrap around the grid
+    button_grid[row * 7 + col].config(text="")
 
 
 
